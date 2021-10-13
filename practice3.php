7bda094448533c8bc361cde8d80da8d62fa8c2dc
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-13 21:57:15 +0900

    [for文]

--- practice3.php
+++ practice3.php
@@ -56,3 +56,40 @@ while($i <= $input){
 }
 ?>
 </select>
+
+//for文でループをスマートに
+<?php
+
+for($i = 1; $i <= 10; $i++){
+    echo $i . "\n";
+}
+
+?>
+
+<?php
+for($i = 1; $i <= 20; $i++) {
+    echo $i . "\n";
+}
+?>
+
+//10以上の際にループがまわる書き方
+<?php
+for($i=20; $i>=10; $i--){
+    echo $i . "\n"
+}
+
+?>
+
+//標準入力で二つ（２行）の整数が与えられます。
+1行目の数値から２行目の数値までを1づつ加算しながら
+1行づつ順番に出力するプログラムをfor文で書いてください
+
+<?php
+$input1 = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+for($i=$input1; $i <= $input2; $i++){
+    echo $i . "\n";
+}
+
+?>
+
