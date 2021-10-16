a0b7c23616565d47dd81f22bfa3c30ad8ac2d242
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-16 22:42:20 +0900

    [配列]

--- practice5.php
+++ practice5.php
@@ -30,3 +30,15 @@ print_r($team);
 
 ?>
 
+//配列の基本操作
+<?php
+$a = trim(fgets(STDIN));
+$b = trim(fgets(STDIN)); 
+$c = trim(fgets(STDIN));
+
+// $a,$b,$cを指定の順番で配列$teamに代入してください
+$team = array($b, $c, $a);
+print_r($team);
+
+?>
+
